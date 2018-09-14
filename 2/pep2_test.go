577a0aab4a6dc3c117cfaf0a4fb9e4dd27621c2b
Andrew David Burt <andrewdavid@Andrews-MacBook-Pro.local>
2018-09-14 12:01:41 -0700

    adding tests

--- 2/pep2_test.go
+++ 2/pep2_test.go
@@ -5,33 +5,41 @@ import (
 	"fmt"
 )
 
-func TestMultiple(t *testing.T) {
-	var n int = 10
-	var m []int
-//	var c []int
-        var c = make([]int, 5)
-	c[0] = 0
-	c[1] = 3
-	c[2] = 5
-	c[3] = 6
-	c[4] = 9
-	m = multiple(n)
-	if len(m) != len(c) {
-		t.Error("Expected length:",len(c), "got length:", len(m))
-//		return 
+func Test_fib(t *testing.T) {
+	var f []int
+	var n int=10
+	var l int = 100
+	var c = make([]int, 10)
+	c[0] = 1
+	c[1] = 2
+	c[2] = 3
+	c[3] = 5
+	c[4] = 8
+	c[5] = 13
+	c[6] = 21
+	c[7] = 34
+	c[8] = 55
+	c[9] = 89
+	f = fib(n, l)
+	if len(c) != len(f) {
+		t.Error("Expected length:", len(c), "got length:", len(f))
 	}
-	for i, v:= range m {
+	for i, v:= range f {
 		if v != c[i] {
-		fmt.Println(v, c[i])
-		t.Error("Expected:", c[i], "got:", v)
+			fmt.Println(v, c[i])
+			t.Error("Expected:", c[i], "got:", v)
 		}
 	}
 }
-
-func TestSum(t *testing.T) {
-	var n int = 10
-	var x = sum_m(n)
-	if (x != 23) {
-		t.Error("Expected 23, got:", x)
+		
+func Test_addevens(t *testing.T) {
+	var f []int
+	var n int=10
+	var l int = 100
+	f = fib(n, l)
+	var ae int
+	ae = addevens(f)
+	if (ae != 231) {
+		t.Error("Expected 231, got:", ae)
 	}
 }
